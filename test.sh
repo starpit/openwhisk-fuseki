11dced9961245839c183d8cbb8c665b40311aa66
Nick M. Mitchell <nickm@us.ibm.com>
2017-01-04 20:38:46 -0500

    update test.sh to echo PASS or FAIL

--- test.sh
+++ test.sh
@@ -4,3 +4,11 @@ wsk action invoke fuseki -b -r \
     -p query "`base64 examples/q-ds-1.rq`" \
     -p model "`base64 examples/ds-dft.ttl`" \
     -p lang ttl
+
+if [ $? == 0 ]; then
+    echo "PASS"
+    exit 0
+else
+    echo "FAIL"
+    exit 1
+fi
