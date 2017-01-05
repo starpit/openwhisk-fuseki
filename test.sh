2e89e89c767d1edfe3ff19339324c5deca90263f
Nick M. Mitchell <nickm@us.ibm.com>
2017-01-04 21:28:49 -0500

    fix test.sh for travis

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-wsk action invoke fuseki -b -r \
+WSK=${WSK-wsk}
+
+$WSK action invoke fuseki -b -r \
     -p query "`base64 examples/q-ds-1.rq`" \
     -p model "`base64 examples/ds-dft.ttl`" \
     -p lang ttl
